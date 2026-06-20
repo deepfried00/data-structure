b85b596f5d22584781193ab09a51a11d54ef900f
deepfried00 <120401193+deepfried00@users.noreply.github.com>
2026-06-21 00:38:31 +0600

    rotate by 90 and 180 +-

--- src/Arrays.java
+++ src/Arrays.java
@@ -6,14 +6,7 @@ public class Arrays{
                 {-6,16,9},
                 {4,13,11},
         };
-        for(int i = 0; i< arr.length; i++){
-            for(int j = 0; j <(arr[0].length)/2; j++){ // m/2= m
-                int temp = arr[i][j]; // 1
-                arr[i][j] = arr[i][(arr[0].length-1-j)]; //1
-                arr[i][(arr[0].length-1-j)] = temp; //1
-            }
-        }
-
+        int[][] swp = swapp(arr);
         // multiplication
         for(int i = 0; i < arr.length; i++){
             for(int j = 0; j < arr[0].length;j++){
@@ -38,22 +31,67 @@ public class Arrays{
 
                 }
         }
+        // transpose
         int[][] an = {
                 {1,2,3},
                 {2,3,4},
                 {4,6,6},
         };
+        int[][] at = rotant(an);
+        // rotate by +90
+        printt(at);
+        System.out.println();
+        int[][] ate = rotcloc(an);
+        printt(ate);
+        System.out.println();
+// rotate +180
+        int[][] te = rotant(rotant(an));
+        printt(te);
+        System.out.println();
+        // rotate -180
+        int[][] ep = rotcloc(rotcloc(an));
+        printt(ep);
+
+    }
+    public static int[][] tranpose(int[][] an){
         int[][] at = new int[(an[0].length)][an.length];
         for(int i = 0; i< at[0].length; i++){
             for(int j = 0; j < at.length;j++){
                 at[j][i] = an[i][j];
             }
         }
-
+        return at;
+    }
+    public static int[][] swapp(int[][] arr){
+        int[][] at = new int[arr.length][arr[0].length];
+        for(int i = 0; i< at.length; i++){
+            for(int j = 0; j < at[0].length;j++){
+                at[i][j] = arr[i][j];
+            }
+        }
+        for(int i = 0; i< at.length; i++){
+            for(int j = 0; j <(at[0].length)/2; j++){ // m/2= m
+                int temp = at[i][j]; // 1
+                at[i][j] = arr[i][(at[0].length-1-j)]; //1
+                at[i][(at[0].length-1-j)] = temp; //1
+            }
+        }
+        return at;
+    }
+    public static void printt(int[][] at){
         for(int i = 0; i < at.length; i++){
             for(int j = 0; j < at[0].length;j++){
                 System.out.print(at[i][j]+ " ");
             }
             System.out.println();
         }
-    }}
+    }
+    public static int[][] rotant(int[][] at){
+    int[][] a = swapp(tranpose(at));
+    return a;
+    }
+    public static int[][] rotcloc(int[][] at){
+        int a[][] = tranpose(swapp(at));
+        return a;
+    }
+}
